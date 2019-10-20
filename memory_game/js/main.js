12805233c8719485f8d0fd4dc861ed67820e9dc7
Rebecca Evans <evans.rebeccaj@gmail.com>
2019-10-19 21:13:28 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,25 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+// console.log("Up and running!");
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne;
+
+cardOne = cards[0];
+cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay [1]) {
+		alert("You found a match!");
+	}
+		else {
+			alert("Sorry, try again")
+	}
+}
