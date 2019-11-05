bc5b95043b5120fdee12fb9063d9b5cafe125004
Rebecca Evans <evans.rebeccaj@gmail.com>
2019-11-05 01:32:37 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,7 @@
 // console.log("Up and running!");
 // console.log("User flipped " + cardOne);
 // console.log("User flipped " + cardTwo);
+
 var cards = [
 {
 	rank: "queen",
@@ -37,17 +38,32 @@ function checkForMatch() {
 	}	
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute("data-id");
+	this.setAttribute("src", cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-
+	//console.log(cardsInPlay);
 	if (cardsInPlay.length === 2) {
-		checkForMatch();
-	}	
+		setTimeout(checkForMatch, 800);
+	//checkForMatch();
+	}
 }
 
-flipCard(0);
-flipCard(2);
-console.log(cardsInPlay);
+function createBoard() {
+	for (var i = 0; i <cards.length; i++) {
+		//logic here
+
+	var cardElement = document.createElement('img');
+
+	cardElement.setAttribute("src", "images/back.png");
+	cardElement.setAttribute("data-id", i);
+	cardElement.addEventListener("click", flipCard);
+	document.getElementById("game-board").appendChild(cardElement);
+} 
+};
+
+
+createBoard();
